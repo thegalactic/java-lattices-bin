4622bc9c1c1316d02dd0eefaec1ae25a5c11fbfc
Christophe Demko <chdemko@gmail.com>
2014-10-30 18:15:26 +0100

    Adding context2lattice application

---
+++ README.md
@@ -0,0 +1,3 @@
+java-lattices-bin ![Snapshot](http://img.shields.io/badge/snapshot-2.0.0-orange.svg) [![License](http://img.shields.io/badge/license-CeCILL--B-red.svg)](http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html)
+==============
+
---
+++ src/test/java/fr/kbertet/app/Context2LatticeTest.java
@@ -0,0 +1,17 @@
+package fr.kbertet.Context2Lattice;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple Context2Lattice.
+ */
+public class Context2LatticeTest {
+    /**
+     * Rigourous Test :-).
+     */
+    @Test
+    public void testContext2Lattice() {
+        assertTrue(true);
+    }
+}
